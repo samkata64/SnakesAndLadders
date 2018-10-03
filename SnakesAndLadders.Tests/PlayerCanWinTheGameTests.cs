cbf5c337ab81400a6aacecf68e8a8441c840e953
samkata64 <samkata64@gmail.com>
2018-10-03 21:54:48 +0100

    Refactor into a steup and rename for clarity

--- SnakesAndLadders.Tests/PlayerCanWinTheGameTests.cs
+++ SnakesAndLadders.Tests/PlayerCanWinTheGameTests.cs
@@ -5,27 +5,29 @@ namespace SnakesAndLadders.Tests
     [TestFixture]
     public class PlayerCanWinTheGameTests
     {
-        [Test]
-        public void GivenTheTokenIsOnSquareNinetySevenWhenTheTokenIsMovedThreeSpacesThenTheTokenIsOnSquareOneHundred()
+        protected internal Game _game;
+        protected internal Token _token;
+
+        [SetUp]
+        public void GivenAGameWithATokenOnSquareNinetySeven()
         {
-            var _game = new Game();
-            var _token = new Token();
+            _game = new Game();
+            _token = new Token();
             _game.Add(_token);
             _game.MoveToken(96);
+        }
 
+        [Test]
+        public void WhenTheTokenIsMovedThreeSpacesTheLocationIsOneHundredAndTheGameIsWon()
+        {
             _game.MoveToken(3);
             Assert.That(_token.Location(), Is.EqualTo(100));
             Assert.That(_game.IsWon(), Is.True);
         }
 
         [Test]
-        public void GivenTheTokenIsOnSquareNinetySevenWhenTheTokenIsMovedThreeSpacesThenTheTokenStaysStill()
+        public void WhenTheTokenIsMovedFourSpacesTheLocationIsUnchangedAndTheGameIsNotWon()
         {
-            var _game = new Game();
-            var _token = new Token();
-            _game.Add(_token);
-            _game.MoveToken(96);
-
             _game.MoveToken(4);
             Assert.That(_token.Location(), Is.EqualTo(97));
             Assert.That(_game.IsWon(), Is.False);

525fd0bca1384eab16d3751ddd51bfcd7d313e98
samkata64 <samkata64@gmail.com>
2018-10-03 22:16:35 +0100

    introduce a Die class, isolated for now

---
+++ SnakesAndLadders.Tests/MovesAreDeterminedByDiceRollsTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq.Expressions;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using NUnit.Framework.Internal;
+
+namespace SnakesAndLadders.Tests
+{
+    [TestFixture]
+    public class MovesAreDeterminedByDiceRollsTests
+    {
+        [Test]
+        public void DiceRollsAreBetweenOneAndSix()
+        {
+            Assert.That(new Die().Roll(), Is.InRange(1, 6));
+        }
+    }
+
+    public class Die
+    {
+        public int Roll()
+        {
+            return new Random().Next(1, 6);
+        }
+    }
+}

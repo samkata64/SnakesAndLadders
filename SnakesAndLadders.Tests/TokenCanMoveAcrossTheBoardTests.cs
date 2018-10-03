ae874173777d849e441d3e19d60f912767585025
samkata64 <samkata64@gmail.com>
2018-10-03 21:07:43 +0100

    adds test that token is on square one at the start

---
+++ SnakesAndLadders.Tests/TokenCanMoveAcrossTheBoardTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace SnakesAndLadders.Tests
+{
+    [TestFixture]
+    public class TokenCanMoveAcrossTheBoardTests
+    {
+        [Test]
+        public void WhenTheTokenIsPlacedOnTheBoardTheTokenIsAtSquareOne()
+        {
+            var game = new Game();
+            var token = new Token();
+            game.Add(token);
+            Assert.That(token.Location(), Is.EqualTo(1));
+        }
+    }
+
+    public class Token
+    {
+        public int Location()
+        {
+            return 1;
+        }
+    }
+
+    public class Game
+    {
+        public void Add(Token token)
+        {
+        }
+    }
+}

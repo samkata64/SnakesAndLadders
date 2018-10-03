086ff5ff2d1e943e7c0caf19ce51b206677cc15c
samkata64 <samkata64@gmail.com>
2018-10-03 22:17:24 +0100

    move Die class to lib

--- SnakesAndLadders.Tests/MovesAreDeterminedByDiceRollsTests.cs
+++ SnakesAndLadders.Tests/MovesAreDeterminedByDiceRollsTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Linq.Expressions;
-using NUnit.Framework;
-using NUnit.Framework.Constraints;
-using NUnit.Framework.Internal;
+﻿using NUnit.Framework;
 
 namespace SnakesAndLadders.Tests
 {
@@ -13,14 +9,6 @@ namespace SnakesAndLadders.Tests
         public void DiceRollsAreBetweenOneAndSix()
         {
             Assert.That(new Die().Roll(), Is.InRange(1, 6));
-        }
-    }
-
-    public class Die
-    {
-        public int Roll()
-        {
-            return new Random().Next(1, 6);
         }
     }
 }
---
+++ SnakesAndLadders/Die.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakesAndLadders
+{
+    public class Die
+    {
+        public int Roll()
+        {
+            return new Random().Next(1, 6);
+        }
+    }
+}

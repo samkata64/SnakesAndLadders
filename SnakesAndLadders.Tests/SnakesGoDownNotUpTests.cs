4407dc6d5fb87177551d785d95498ff17ccf9c46
samkata64 <samkata64@gmail.com>
2018-10-03 22:54:18 +0100

    refactor to a Setup and rename tests for clarity

--- SnakesAndLadders.Tests/SnakesGoDownNotUpTests.cs
+++ SnakesAndLadders.Tests/SnakesGoDownNotUpTests.cs
@@ -5,18 +5,32 @@ namespace SnakesAndLadders.Tests
     [TestFixture]
     public class SnakesGoDownNotUpTests
     {
-        [Test]
-        public void GivenASnakeBetweenTwoAndTwelveWhenTheTokenLandsOnTwelveThenTheTokenEndsOnTwo()
+        private Game _game;
+        private Token _token;
+
+        [SetUp]
+        public void GivenAGameWithASnakeBetweenTwelveAndTwo()
         {
-            var game = new Game();
-            var token = new Token();
-            game.Add(token);
-            game.Add(new Snake(12, 2));
+            _game = new Game();
+            _token = new Token();
+            _game.Add(_token);
+            _game.Add(new Snake(12, 2));
+        }
 
-            game.MoveToken(6);
-            game.MoveToken(5);
+        [Test]
+        public void WhenTheTokenLandsOnTwelveThenTheTokenEndsOnTwo()
+        {
+            _game.MoveToken(6);
+            _game.MoveToken(5);
 
-            Assert.That(token.Location(), Is.EqualTo(2));
+            Assert.That(_token.Location(), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void WhenTheTokenIsOnSquareTwoItStaysOnSquareTwo()
+        {
+            _game.MoveToken(1);
+            Assert.That(_token.Location(), Is.EqualTo(2));
         }
     }
 }
